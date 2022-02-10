64cb0b866ed5589594a821eb26b7cce5294f4ceb
ArchitBorde <archit.borde@gmail.com>
2022-02-11 00:47:15 +0530

    btn added

---
+++ app.js
@@ -0,0 +1,13 @@
+
+var btn_translator = document.querySelector("#btn-translate");
+
+function clickHandler() {
+    console.log("Clicked!");
+};
+
+
+btn_translator.addEventListener("click", clickHandler)
+
+
+
+
