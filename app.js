e97d70e3b6f30b7e3d643d787edc0d896f3b4ae2
ArchitBorde <archit.borde@gmail.com>
2022-02-14 01:43:49 +0530

    banana  app vanilla js done

--- app.js
+++ app.js
@@ -2,9 +2,29 @@
 var btn_translator = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
 var outputText = document.querySelector("#output");
+//var sererURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function errorHandler(error){
+    console.log("Error occured "+error);
+    alert("Something went wrong with the Server. Please try again in sometime");
+}
+
+function getTranslationUrl(text) {
+    return serverURL + "?" + "text=" +text;
+}
 
 function clickHandler() {
-    outputText.innerText = "abifvwubevuobqef " +textInput.value;
+    
+    var txtInput = textInput.value;
+    fetch(getTranslationUrl(txtInput))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputText.innerText = translatedText;
+    })
+    .catch(errorHandler);
+
 };
 
 
